92403cb43900f9a98231078ad9f7dc1ff1a549ea
nikita.feoktistov <nikitos.feo12@gmail.com>
2018-12-17 16:45:08 +0700

    Lab 2.1 Rabin carp - all tests

--- Rabincarp.cpp
+++ Rabincarp.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include<Windows.h>
 #include <string.h>
+#include<math.h>
 #include <stdlib.h>
-#define MAXPAT 18
-int power(int i) {
+int pow(int i) {
 	int prod = 1;
 	while (i != 0) {
 		prod *= 3;
@@ -11,19 +12,22 @@ int power(int i) {
 	return prod;
 }
 long Hash(char *pattern, int length) {
-	long h = 0;
-	for (int i = 0; i < length; i++)
-		h += (pattern[i] % 3) * (power(i));
+	long long h = 0;
+		for (int i = 0; i < length; i++)
+		h +=(((unsigned int)(pattern[i])) % 3) * (pow(i));
 	return h;
 }
 int main() {
-	char pattern[MAXPAT] = { 0 };
-	char substring[MAXPAT] = { 0 };
-	fgets(pattern, MAXPAT, stdin);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int maxpattern = 18;
+	char pattern[maxpattern] = { 0 };
+	char substring[maxpattern] = { 0 };
+	fgets(pattern, maxpattern, stdin);
 	int length = strlen(pattern) - 1;
 	int Mainhash = Hash(pattern, length);
 	unsigned char letter;
-	int MaxP = power(length - 1);
+	int MaxP = pow(length - 1);
 	fgets(substring, length + 1, stdin);
 	int SubHash = Hash(substring, length);
 	printf("%d ", Mainhash);
@@ -36,7 +40,7 @@ int main() {
 			}
 		}
 		letter = fgetc(stdin);
-		SubHash = (SubHash - (substring[i % length] % 3)) / 3 + ((letter % 3) * MaxP);
+		SubHash = (SubHash - ((unsigned int)substring[i % length] % 3)) / 3 + ((letter % 3) * MaxP);
 		substring[i % length] = letter;
 	}
 	return 0;
